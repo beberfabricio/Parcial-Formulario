a8a159dec524578b520d62a3e481d348541a938d
beberfabricio <beberfabricio@gmail.com>
2022-06-07 11:15:43 -0300

    detalles en JS y CSS

--- scripts/script.js
+++ scripts/script.js
@@ -2,34 +2,11 @@ window.onload = () => {
     removerLabelTxt(document.querySelectorAll("input[type=text]"));
     removerLabelRadio(document.querySelectorAll("input[type=radio]"));
     removerLabelCheck(document.querySelectorAll("input[type=checkbox]"));
+    removerLabelPais(document.getElementById("cmbPaises"));
     document.getElementById("btnAceptar").addEventListener("click", function (e) {
         e.preventDefault();
         validarCampos();
       });
-}
-
-function removerLabelTxt(txt) {
-    for (let i=0; i<txt.length; i++){
-        txt[i].addEventListener("focus",function(){
-            txt[i].labels[1].classList.toggle("hidden",true)
-        });
-    }
-}
-
-function removerLabelRadio(rb) {
-    for (let i=0; i<rb.length; i++){
-        rb[i].addEventListener("click",function(){
-            document.getElementById("lblErrorS").classList.toggle("hidden",true);
-        });
-    }
-}
-
-function removerLabelCheck(chk) {
-    for (let i=0; i<chk.length; i++){
-        chk[i].addEventListener("click",function(){
-            document.getElementById("lblErrorT").classList.toggle("hidden",true);
-        });
-    }
 }
 
 function validarCampos() {
@@ -38,6 +15,8 @@ function validarCampos() {
     validarEmail(document.getElementById("txtEmail"));
     validarEdad(document.getElementById("txtEdad"));
     validarSexo(document.getElementsByName("rbSexo"));
+    validarTemas(document.getElementsByName("cbTemas"));
+    validarPais(document.getElementById("cmbPaises"));
 }
 
 function validarNombre(txt) {
@@ -64,12 +43,50 @@ function validarEdad(txt) {
     }
 }
 
-function validarSexo(rb){
+function validarSexo(rb) {
     if (rb[0].checked == false && rb[1].checked == false && rb[2].checked == false) {
         document.getElementById("lblErrorS").classList.toggle("hidden",false);
     }
 }
 
-function validarTemas(chk){
+function validarTemas(chk) {
+    if (chk[0].checked == false && chk[1].checked == false && chk[2].checked == false && chk[3].checked == false && chk[4].checked == false) {
+        document.getElementById("lblErrorT").classList.toggle("hidden",false);
+    }
+}
 
+function validarPais(sel) {
+    if (sel.selectedIndex == 0) {
+        document.getElementById("lblErrorP").classList.toggle("hidden",false);
+    }
+}
+
+function removerLabelTxt(txt) {
+    for (let i=0; i<txt.length; i++){
+        txt[i].addEventListener("focus",function(){
+            txt[i].labels[1].classList.toggle("hidden",true)
+        });
+    }
+}
+
+function removerLabelRadio(rb) {
+    for (let i=0; i<rb.length; i++){
+        rb[i].addEventListener("click",function(){
+            document.getElementById("lblErrorS").classList.toggle("hidden",true);
+        });
+    }
+}
+
+function removerLabelCheck(chk) {
+    for (let i=0; i<chk.length; i++){
+        chk[i].addEventListener("click",function(){
+            document.getElementById("lblErrorT").classList.toggle("hidden",true);
+        });
+    }
+}
+
+function removerLabelPais(sel) {
+    sel.addEventListener("focus",function(){
+        document.getElementById("lblErrorP").classList.toggle("hidden",true);
+    })
 }
